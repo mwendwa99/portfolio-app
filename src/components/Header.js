2924cc35cb0198a4035d4cf592d33658c0538b4e
mwendwa99 <brianmwendwa1999@gmail.com>
2021-02-15 16:00:49 +0300

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1> web development and websites promotions </h1>
+        <Typed
+          className="typed-text"
+          strings={["web design", "Web Development", "Android Apps"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          Contact Me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
